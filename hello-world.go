d71b3ef4df4adac14414adcf35054d9a40115424
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-08-03 22:42:39 +0530

    Added switch case

--- hello-world.go
+++ hello-world.go
@@ -9,15 +9,19 @@ const spanishHelloPrefix = "Hola, "
 const frenchhHelloPrefix = "Bonjour, "
 
 func Hello(name, language string) string {
+
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	} else if language == french {
-		return frenchhHelloPrefix + name
+	prefix := englishHelloPrefix
+	switch language {
+	case french:
+		prefix = frenchhHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
 	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 
 }
 
