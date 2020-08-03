01a0bdc99794911f90c3b824c11fbdb6370e53b5
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-08-03 22:33:19 +0530

    spanish added

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "English")
+		want := "Hello, Chris"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World"
+		if got != want {
+			assertCorrectMessage(t, got, want)
+
+		}
+
+	})
+	t.Run("sain Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		if got != want {
+			assertCorrectMessage(t, got, want)
+
+		}
+
+	})
+
 }
