ca222a94795d9e15764596dc833fac4e298c3161
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-08-05 22:29:32 +0530

    Added interface

--- Structure/shapes_test.go
+++ Structure/shapes_test.go
@@ -15,23 +15,21 @@ func TestPerimeter(t *testing.T) {
 	})
 }
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g and want %g", got, want)
+
+		}
+	}
 	t.Run("Testing rectangle area", func(t *testing.T) {
 		rectangle := Rectangle{20.0, 40.0}
-		got := rectangle.Area()
-		want := 800.0
-		if got != want {
-			t.Errorf("got %g and want %g", got, want)
-		}
+		checkArea(t, rectangle, 800.0)
 	})
-}
-
-func TestCircleArea(t *testing.T) {
 	t.Run("Testing circle area", func(t *testing.T) {
 		circle := Circle{20.0}
-		got := circle.Area()
-		want := 1256.6370614359173
-		if got != want {
-			t.Errorf("got %g and want %g", got, want)
-		}
+		checkArea(t, circle, 1256.6370614359173)
+
 	})
 }
