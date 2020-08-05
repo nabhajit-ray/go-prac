d3b22c4ec08201f75a20e3c601f55675d5e3099d
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-08-05 21:12:40 +0530

    circle added

--- Structure/shape.go
+++ Structure/shape.go
@@ -1,11 +1,25 @@
 package main
 
-func Perimeter(length float64, breadth float64) float64 {
+import "math"
 
-	return 2 * (length + breadth)
+type Rectangle struct {
+	length  float64
+	breadth float64
 }
 
-func Area(length float64, breadth float64) float64 {
+type Circle struct {
+	radius float64
+}
 
-	return length * breadth
+func Perimeter(r Rectangle) float64 {
+
+	return 2 * (r.length + r.breadth)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.breadth * r.length
+
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
--- Structure/shapes_test.go
+++ Structure/shapes_test.go
@@ -4,8 +4,9 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	t.Run("Testing rectangle shape", func(t *testing.T) {
-		got := Perimeter(10.0, 20.0)
+	t.Run("Testing rectangle perimeter", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 20.0}
+		got := Perimeter(rectangle)
 		want := 60.0
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
@@ -14,9 +15,23 @@ func TestPerimeter(t *testing.T) {
 	})
 }
 func TestArea(t *testing.T) {
-	got := Area(10.0, 30.0)
-	want := 300.0
-	if got != want {
-		t.Errorf("got %.2f and want %.2f", got, want)
-	}
+	t.Run("Testing rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{20.0, 40.0}
+		got := rectangle.Area()
+		want := 800.0
+		if got != want {
+			t.Errorf("got %g and want %g", got, want)
+		}
+	})
+}
+
+func TestCircleArea(t *testing.T) {
+	t.Run("Testing circle area", func(t *testing.T) {
+		circle := Circle{20.0}
+		got := circle.Area()
+		want := 1256.6370614359173
+		if got != want {
+			t.Errorf("got %g and want %g", got, want)
+		}
+	})
 }
