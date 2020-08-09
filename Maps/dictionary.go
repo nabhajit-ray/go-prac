20a6492e41907fb311259c8ebd768f8d63e14fe9
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-08-09 16:54:43 +0530

    added delete

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -1,5 +1,42 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrorNotFound = errors.New("Word not found")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if ok != true {
+		return "", ErrorNotFound
+
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+
+	d[word] = definition
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, ok := d[word]
+	if ok != true {
+		return ErrorNotFound
+	}
+	d[word] = definition
+	return nil
+
+}
+
+func (d Dictionary) Delete(word string) error {
+
+	_, ok := d[word]
+	if ok != true {
+		return ErrorNotFound
+	} else {
+		delete(d, word)
+		return nil
+	}
 }
--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -3,20 +3,71 @@ package main
 import "testing"
 
 func TestDictionary(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("Checking word find", func(t *testing.T) {
 
-		dictionary := map[string]string{"test": "this is just a test"}
-
-		got := Search(dictionary, "test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
 
 	})
+	t.Run("Checking unknown word find", func(t *testing.T) {
+
+		//dictionary := Dictionary{"test": "this is just a test"}
+
+		_, got := dictionary.Search("unknown_word")
+		//want := "word not found"
+
+		assertError(t, got, ErrorNotFound)
+
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test2", "This is test2")
+	want := "This is test2"
+	got, err := dictionary.Search("test2")
+	if err != nil {
+		t.Fatal("Something weird happended,could not add")
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
+
+func TestUpdate(t *testing.T) {
+
+	dictionary := Dictionary{"test": "this is test"}
+	dictionary.Update("test", "this is a test2")
+	want := "this is a test2"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("Unexpected! could not update")
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+func TestDelete(t *testing.T) {
+	dictionary := Dictionary{"test": "this is test"}
+	dictionary.Delete("test")
+	_, got := dictionary.Search("test")
+	assertError(t, got, ErrorNotFound)
+
 }
 
 func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
