893396f78a5efca34004b8c79b0b9d32d392600b
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-09-30 21:03:48 +0530

    intial  commit

--- select/race.go
+++ select/race.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
-)
+import "net/http"
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
 
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
-
-func measureResponseTime(url string) time.Duration {
-	fmt.Println(url)
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
